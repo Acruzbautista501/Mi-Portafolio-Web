e3c2e3031fbbbb2e9604672c7770dba24e5f24b6
Acruzbautista501 <acruz.pbxhosting@gmail.com>
2026-02-19 13:36:43 -0600

    Creacion del componente Header

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HeaderComponent from './components/HeaderComponent.vue';
+</script>
+
+<template>
+  <HeaderComponent />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <CHeader class="border-bottom border-dark mb-4">
+    <CContainer fluid class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-between py-3">
+      <div class="col-lg-3 mb-2 mb-lg-0">
+        <a
+          href="/"
+          class="d-inline-flex align-items-center text-decoration-none text-body"
+        >
+          <div class="border p-2 bg-dark me-1">
+            <font-awesome-icon icon="terminal" size="lg" class="text-white"/>
+          </div>
+          
+          <span class="fw-semibold fs-4">Aldair</span>
+        </a>
+      </div>
+
+      <CHeaderNav class="col-12 col-lg-auto mb-2 justify-content-center mb-lg-0">
+        <CNavItem>
+          <CNavLink href="#" active>Acerca de Mi</CNavLink>
+        </CNavItem>
+        <CNavItem>
+          <CNavLink href="#">Habilidades</CNavLink>
+        </CNavItem>
+        <CNavItem>
+          <CNavLink href="#">Proyectos</CNavLink>
+        </CNavItem>
+        <CNavItem>
+          <CNavLink href="#">Contacto</CNavLink>
+        </CNavItem>
+      </CHeaderNav>
+      <div class="col-lg-3 text-end">
+        <CButton color="dark">
+          <font-awesome-icon icon="file-invoice" class="text-white"/> Descargar CV
+        </CButton>
+      </div>
+    </CContainer>
+  </CHeader>
+</template>
+
+<style scoped>
+
+</style>
