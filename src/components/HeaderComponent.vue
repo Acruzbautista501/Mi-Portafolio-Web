7c70e068c64f3c8e8df66fca30b903bf7b645ee6
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2026-02-28 00:10:23 -0600

    Actualizacion de Header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,55 +1,68 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const visible = ref(false)
 </script>
 
 <template>
-  <CHeader class="border-bottom border-dark mb-4">
-    <CContainer class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-between py-3">
-      <div class="col-lg-3 mb-2 mb-lg-0">
-        <a
-          href="/"
-          class="d-inline-flex align-items-center text-decoration-none text-body"
-        >
-          <div class="border p-2 bg-dark me-1">
+  <CHeader class="border-bottom border-dark mb-4 p-0">
+    <CNavbar expand="lg" color-scheme="light" class="w-100">
+      <CContainer class="px-4">
+        <CNavbarBrand href="/" class="d-flex align-items-center">
+          <div class="border p-2 bg-dark me-2">
             <font-awesome-icon icon="terminal" size="lg" class="text-white"/>
           </div>
-          
-          <span class="fw-semibold fs-4">Aldair</span>
-        </a>
-      </div>
+          <span class="fw-semibold fs-4 text-dark">Aldair</span>
+        </CNavbarBrand>
 
-      <CHeaderNav class="col-12 col-lg-auto mb-2 justify-content-center mb-lg-0">
-        <CNavItem>
-          <CNavLink href="#tech">Tecnologías</CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#exp">Experiencia</CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#about-Me">Acerca de Mi</CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#projects">Proyectos</CNavLink>
-        </CNavItem>
-        <CNavItem>
-          <CNavLink href="#contact">Contacto</CNavLink>
-        </CNavItem>
-      </CHeaderNav>
-      <div class="col-lg-3 text-end">
-        <CButton
-          color="dark"
-          component="a"
-          href="/files/Aldair_Cruz_Bautista_CV.pdf"
-          download="Aldair_Cruz_Bautista_CV.pdf"
-        >
-          <font-awesome-icon icon="file-invoice" class="text-white"/>
-          Descargar CV
-        </CButton>
-      </div>
-    </CContainer>
+        <CNavbarToggler @click="visible = !visible" />
+
+        <CCollapse class="navbar-collapse text-center" :visible="visible">
+          <CNavbarNav class="mx-auto mb-2 mb-lg-0">
+            <CNavItem>
+              <CNavLink href="#tech">Tecnologías</CNavLink>
+            </CNavItem>
+            <CNavItem>
+              <CNavLink href="#exp">Experiencia</CNavLink>
+            </CNavItem>
+            <CNavItem>
+              <CNavLink href="#about-Me">Acerca de Mi</CNavLink>
+            </CNavItem>
+            <CNavItem>
+              <CNavLink href="#projects">Proyectos</CNavLink>
+            </CNavItem>
+            <CNavItem>
+              <CNavLink href="#contact">Contacto</CNavLink>
+            </CNavItem>
+          </CNavbarNav>
+
+          <div class="d-grid d-lg-block">
+            <CButton
+              color="dark"
+              component="a"
+              href="/files/Aldair_Cruz_Bautista_CV.pdf"
+              download="Aldair_Cruz_Bautista_CV.pdf"
+              class="text-nowrap"
+            >
+              <font-awesome-icon icon="file-invoice" class="me-2 text-white"/>
+              Descargar CV
+            </CButton>
+          </div>
+        </CCollapse>
+      </CContainer>
+    </CNavbar>
   </CHeader>
 </template>
 
 <style scoped>
+/* Ajuste para que el nav no se pegue a los bordes en móviles */
+.navbar-collapse {
+  padding: 1rem 0;
+}
 
+@media (min-width: 992px) {
+  .navbar-collapse {
+    padding: 0;
+  }
+}
 </style>
